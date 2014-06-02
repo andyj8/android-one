f8a7cbe1f042fb1850942e777da6480eacf125f9
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-02 11:03:34 +0100

    Add settings activity

---
+++ main/java/com/andy/auth/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.andy.auth;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+            .replace(android.R.id.content, new SettingsFragment())
+            .commit();
+    }
+}
