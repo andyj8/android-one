00968aa0a00829733bb86d3eb13e879e004a2b28
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-02 11:13:07 +0100

    Register new activities

--- main/java/com/andy/auth/MainActivity.java
+++ main/java/com/andy/auth/MainActivity.java
@@ -2,8 +2,8 @@ package com.andy.auth;
 
 import android.accounts.*;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -25,26 +25,27 @@ public class MainActivity extends Activity {
             mAccountManager.addAccount("com.andy.auth", "FULL", null, null, this, null, null);
         }
 
-        finish();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(i);
+            return true;
+        } else if (id == R.id.action_about) {
+            Intent i = new Intent(getApplicationContext(), AboutActivity.class);
+            startActivity(i);
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
