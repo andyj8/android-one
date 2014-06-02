68ecedf91228d58e398d238b2d82c2c4a1a5d51c
Andrew Brooks <andrew.brooks@talktalkplc.com>
2014-06-02 16:56:52 +0100

    Add drawer setup

--- main/java/com/andy/auth/MainActivity.java
+++ main/java/com/andy/auth/MainActivity.java
@@ -3,7 +3,9 @@ package com.andy.auth;
 import android.accounts.*;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,37 +14,20 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
+
     private String[] mLinks;
     private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
     private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AccountManager mAccountManager = AccountManager.get(this);
-        Account[] accounts = mAccountManager.getAccountsByType("com.andy.auth");
-
-        if (accounts.length > 0) {
-            Account account = accounts[0];
-            mAccountManager.getAuthToken(account, "FULL", null, this, null, null);
-        } else {
-            mAccountManager.addAccount("com.andy.auth", "FULL", null, null, this, null, null);
-        }
-
-        mLinks = getResources().getStringArray(R.array.drawer_links);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-        mDrawerList.setAdapter(new ArrayAdapter<String>(
-            this, android.R.layout.simple_list_item_1, mLinks
-        ));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
+        authenticate();
+        setupDrawer();
     }
 
     @Override
@@ -53,6 +38,10 @@ public class MainActivity extends Activity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
@@ -67,11 +56,73 @@ public class MainActivity extends Activity
         return super.onOptionsItemSelected(item);
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener
-    {
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * Authenticate the user.
+     */
+    private void authenticate() {
+        AccountManager mAccountManager = AccountManager.get(this);
+        Account[] accounts = mAccountManager.getAccountsByType("com.andy.auth");
+
+        if (accounts.length > 0) {
+            Account account = accounts[0];
+            mAccountManager.getAuthToken(account, "FULL", null, this, null, null);
+        } else {
+            mAccountManager.addAccount("com.andy.auth", "FULL", null, null, this, null, null);
+        }
+    }
+
+    /**
+     * Set up the navigation drawer.
+     */
+    private void setupDrawer() {
+        mLinks = getResources().getStringArray(R.array.drawer_links);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+            this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close
+        ) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void syncState() {
+                super.syncState();
+            }
+        };
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(
+            this, android.R.layout.simple_list_item_1, mLinks
+        ));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
 
         }
     }
